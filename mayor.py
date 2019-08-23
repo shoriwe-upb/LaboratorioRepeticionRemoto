51b417eb4410b1b341ad13dee1cdfd6fb42624c6
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2019-08-22 19:41:38 -0500

    Ejercicio resuelto
    Recieve 10 numbers from user, then count each number that is equal to zero, positive and negative.

--- mayor.py
+++ mayor.py
@@ -1,9 +1,18 @@
 def main():
-    a = float(input("Numero a: "))
-    if a > 0:
-        print("Mayor")
-    else:
-        print("Menor")
+    zeros = 0
+    negatives = 0
+    positives = 0
+    for step in range(10):
+        number = float(input(f"Number {step + 1}: "))
+        if number > 0:
+            positives += 1
+        elif number < 0:
+            negatives += 1
+        else:
+            zeros += 1
+    print(f"Number of zeros: {zeros}")
+    print(f"Number of negatives: {negatives}")
+    print(f"Number of negatives: {positives}")
 
 
 if __name__ == '__main__':
