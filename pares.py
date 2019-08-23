910287e25713982fd35cbc379678cedc14073563
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2019-08-22 19:51:44 -0500

    Ejercicio dos resuelto
    Read from user N numbers and stop reading when the user inputs "0", then show the number of even typed and odd typed

--- pares.py
+++ pares.py
@@ -1,9 +1,16 @@
 def main():
-    a = float(input("Numero a: "))
-    if a % 2 == 0:
-        print("Es par")
-    else:
-        print("Es impar")
+    even_numbers = 0
+    odd_numbers = 0
+    while True:
+        number = float(input(f"Number {even_numbers + odd_numbers + 1}: "))
+        if number == 0:
+            break
+        if number % 2 == 0:
+            even_numbers += 1
+        else:
+            odd_numbers += 1
+    print(f"Numeros pares {even_numbers}")
+    print(f"Numeros impares {odd_numbers}")
 
 
 if __name__ == '__main__':
